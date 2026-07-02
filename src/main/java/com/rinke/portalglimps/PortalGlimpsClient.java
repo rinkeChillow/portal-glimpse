c23e50fadece86eaa60bb626941dedca1ef87aed
rinke <mohamedfriaa69@gmail.com>
2026-07-02 21:33:13 +0100

    inital commit

---
+++ src/main/java/com/rinke/portalglimps/PortalGlimpsClient.java
@@ -0,0 +1,12 @@
+package com.rinke.portalglimps;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalGlimpsClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
